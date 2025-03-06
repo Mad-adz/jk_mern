deef031dfe153786adae4d66499e01726a6ca091
Mad-adz <madadzindia@gmail.com>
2025-03-06 23:32:34 +0530

    added images

---
+++ client/src/assets/images/index.js
@@ -0,0 +1,71 @@
+import jaikosha from "./jaikosha.png";
+import bannerImg1 from "./banner_img1.jpg";
+import bannerImg2 from "./banner_img2.jpg";
+import bannerImg3 from "./banner_img3.jpg";
+import bannerImg4 from "./banner_img4.jpg";
+
+import resourceImg1 from "./resources/1.jpg";
+import resourceImg2 from "./resources/2.jpg";
+import resourceImg3 from "./resources/3.jpg";
+import resourceImg4 from "./resources/4.jpg";
+
+import testimonialImg1 from "./testimonials/1.jpg";
+import testimonialImg2 from "./testimonials/2.jpg";
+import testimonialImg3 from "./testimonials/3.jpg";
+import testimonialImg4 from "./testimonials/4.jpg";
+
+import aboutBanner from "./about_banner.jpg";
+import donationBanner from "./donation_banner.jpg";
+
+import eventBanner from "./event_banner.jpg";
+import eventBanner2 from "./event_banner2.jpg";
+
+import contactBanner from "./contact_banner.jpg";
+
+import workshopBanner from "./workshop_banner.jpg";
+
+import yendhrakovilBanner from "./yendhra_kovil_banner.jpg";
+import meditationBanner from "./meditation_banner.jpg";
+import superiorYogaBanner from "./superior_yoga_banner.jpg";
+import avSermonsBanner from "./av_sermons_banner.jpg";
+import spiritualDevicesBanner from "./spiritual_devices_banner.jpg";
+
+import fullMoonFestival from "./events/fullmoon_festival.jpg";
+import chandiHomam from "./events/chandi_homam.jpg";
+import spiritualClasses from "./events/spiritual_classes.jpg";
+
+import dharshanBanner from "./dharshan_banner.jpg";
+
+import upiQRCode from "./donate/upi_qrcode.jpeg";
+
+export {
+  jaikosha,
+  bannerImg1,
+  bannerImg2,
+  bannerImg3,
+  bannerImg4,
+  resourceImg1,
+  resourceImg2,
+  resourceImg3,
+  resourceImg4,
+  testimonialImg1,
+  testimonialImg2,
+  testimonialImg3,
+  testimonialImg4,
+  aboutBanner,
+  donationBanner,
+  eventBanner,
+  eventBanner2,
+  contactBanner,
+  workshopBanner,
+  yendhrakovilBanner,
+  meditationBanner,
+  superiorYogaBanner,
+  avSermonsBanner,
+  spiritualDevicesBanner,
+  chandiHomam,
+  fullMoonFestival,
+  spiritualClasses,
+  dharshanBanner,
+  upiQRCode,
+};
