f7c1208db66edce91f947d93ed994b3be9931429
Mad-adz <madadzindia@gmail.com>
2025-03-06 23:36:35 +0530

    updated components

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,8 +1,265 @@
+import { Link } from "react-router-dom";
+import { jaikosha } from "../assets/images";
+import {
+  FaFacebookF,
+  FaInstagram,
+  FaYoutube,
+  FaLinkedinIn,
+} from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div>
+      <footer className="bg-emerald-700">
+        <div className="container">
+          <div className="px-6 py-12">
+            <div className="xl:grid xl:grid-cols-3 xl:gap-8">
+              <div className="space-y-4">
+                <div>
+                  <Link to="/">
+                    <div className="flex items-center space-x-2 text-2xl font-medium">
+                      <span>
+                        <img
+                          src={jaikosha}
+                          alt="AI Logo"
+                          width="64"
+                          height="64"
+                          className="w-16"
+                        />
+                      </span>
+                      <span className="text-white">
+                        Jai Kosha <br />
+                        Foundation
+                      </span>
+                    </div>
+                  </Link>
+                </div>
+                <div className="max-w-sm lg:pr-16 text-md text-white">
+                  Enhance productivity and efficiency with cutting-edge
+                  artificial intelligence solutions for your business
+                  operations.
+                </div>
+                <div className="flex space-x-3">
+                  <Link
+                    to=""
+                    // target="_blank"
+                    className="text-white hover:text-white text-xl font-semibold"
+                  >
+                    <span className="sr-only">Facebook</span>
+                    <FaFacebookF />
+                  </Link>
+                  <Link
+                    to=""
+                    // target="_blank"
+                    className="text-white hover:text-white text-xl font-semibold"
+                  >
+                    <span className="sr-only">Instagram</span>
+                    <FaInstagram />
+                  </Link>
+                  <Link
+                    to=""
+                    // target="_blank"
+                    className="text-white hover:text-white text-xl font-semibold"
+                  >
+                    <span className="sr-only">Youtube</span>
+                    <FaYoutube />
+                  </Link>
+                  <Link
+                    to=""
+                    // target="_blank"
+                    className="text-white hover:text-white text-xl font-semibold"
+                  >
+                    <span className="sr-only">Youtube</span>
+                    <FaLinkedinIn />
+                  </Link>
+                  <Link
+                    to=""
+                    // target="_blank"
+                    className="text-white hover:text-white text-xl font-semibold"
+                  >
+                    <span className="sr-only">Youtube</span>
+                    <FaXTwitter />
+                  </Link>
+                </div>
+              </div>
+              <div className="mt-16 grid grid-cols-1 lg:grid-cols-2 gap-8 xl:col-span-2 xl:mt-0">
+                <div className="md:grid md:grid-cols-2 md:gap-8">
+                  <div>
+                    <h3 className="text-md font-semibold leading-6 text-white">
+                      Quick Links
+                    </h3>
+                    <ul role="list" className="mt-6 space-y-4">
+                      <li>
+                        <Link
+                          to="/"
+                          className="text-md leading-6 text-gray-300 hover:text-gray-50"
+                        >
+                          Home
+                        </Link>
+                      </li>
+                      <li>
+                        <Link
+                          to="autobiography"
+                          className="text-md leading-6 text-gray-300 hover:text-gray-50"
+                        >
+                          Autobiography
+                        </Link>
+                      </li>
+                      <li>
+                        <Link
+                          to="contact"
+                          className="text-md leading-6 text-gray-300 hover:text-gray-50"
+                        >
+                          Contact Us
+                        </Link>
+                      </li>
+                    </ul>
+                  </div>
+                  <div className="mt-10 md:mt-0">
+                    <h3 className="text-md font-semibold leading-6 text-white">
+                      Resources
+                    </h3>
+                    <ul role="list" className="mt-6 space-y-4">
+                      <li>
+                        <Link
+                          to="meditation-practices"
+                          className="text-md leading-6 text-gray-300 hover:text-gray-50"
+                        >
+                          Meditation Practices
+                        </Link>
+                      </li>
+                      <li>
+                        <Link
+                          to="superior-yoga"
+                          className="text-md leading-6 text-gray-300 hover:text-gray-50"
+                        >
+                          Superior Yoga
+                        </Link>
+                      </li>
+                      <li>
+                        <Link
+                          to="audio-video-sermons"
+                          className="text-md leading-6 text-gray-300 hover:text-gray-50"
+                        >
+                          Audio & Video Sermons
+                        </Link>
+                      </li>
+                      <li>
+                        <Link
+                          to="spiritual-devices"
+                          className="text-md leading-6 text-gray-300 hover:text-gray-50"
+                        >
+                          Spiritual Devices
+                        </Link>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+                <div className="md:grid md:grid-cols-2 md:gap-8">
+                  <div>
+                    <h3 className="text-md font-semibold leading-6 text-white">
+                      Services
+                    </h3>
+                    <ul role="list" className="mt-6 space-y-4">
+                      <li>
+                        <Link
+                          to=""
+                          className="text-md leading-6 text-gray-300 hover:text-gray-50"
+                        >
+                          Aagamam
+                        </Link>
+                      </li>
+                      <li>
+                        <Link
+                          to=""
+                          className="text-md leading-6 text-gray-300 hover:text-gray-50"
+                        >
+                          Gurukula Vaalkai
+                        </Link>
+                      </li>
+                      <li>
+                        <Link
+                          to=""
+                          className="text-md leading-6 text-gray-300 hover:text-gray-50"
+                        >
+                          Jothidakkalai
+                        </Link>
+                      </li>
+                      <li>
+                        <Link
+                          to=""
+                          className="text-md leading-6 text-gray-300 hover:text-gray-50"
+                        >
+                          Yendra Prayogam
+                        </Link>
+                      </li>
+                      <li>
+                        <Link
+                          to=""
+                          className="text-md leading-6 text-gray-300 hover:text-gray-50"
+                        >
+                          Inner Alignement Yoga
+                        </Link>
+                      </li>
+                    </ul>
+                  </div>
+                  <div className="mt-10 md:mt-0">
+                    <h3 className="text-md font-semibold leading-6 text-white">
+                      Others
+                    </h3>
+                    <ul role="list" className="mt-6 space-y-4">
+                      <li>
+                        <Link
+                          to=""
+                          className="text-md leading-6 text-gray-300 hover:text-gray-50"
+                        >
+                          FAQ
+                        </Link>
+                      </li>
+                      <li>
+                        <Link
+                          to=""
+                          className="text-md leading-6 text-gray-300 hover:text-gray-50"
+                        >
+                          Blogs
+                        </Link>
+                      </li>
+                      <li>
+                        <Link
+                          to=""
+                          className="text-md leading-6 text-gray-300 hover:text-gray-50"
+                        >
+                          Privacy Policy
+                        </Link>
+                      </li>
+                      <li>
+                        <Link
+                          to=""
+                          className="text-md leading-6 text-gray-300 hover:text-gray-50"
+                        >
+                          Terms & Conditions
+                        </Link>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="mt-16 border-t border-gray-400/30 pt-8 sm:mt-20 lg:mt-24">
+              <div className="text-md text-center text-white">
+                Copyright © 2024 - Jai Kosha. All Rights Reserved.
+                {/* Crafted with <span className="text-gray-50">♥</span> by AI enthusiasts at
+                <a rel="noopener" href="/">
+                  AIOps.
+                </a> */}
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,5 +1,238 @@
+import { useState } from "react";
+import { useLocation } from "react-router-dom";
+import {
+  LuChevronDown,
+  LuMenu,
+  // LuUser,
+  // LuSettings,
+  // LuLogOut,
+} from "react-icons/lu";
+
+import { NavLink } from "react-router-dom";
+import { jaikosha } from "../assets/images";
+import { navLinks } from "@/constants";
+
 const Header = () => {
-  return <div>Header</div>;
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [openDropdown, setOpenDropdown] = useState({
+    about: false,
+    initiatives: false,
+    profile: false,
+  });
+  const location = useLocation();
+
+  const toggleMenu = () => {
+    setIsMenuOpen((prev) => !prev);
+  };
+
+  const handleDropdown = (key) =>
+    setOpenDropdown((prev) => ({ ...prev, [key]: !prev[key] }));
+
+  // const navLinks = [
+  //   { id: 1, to: "", label: "Home", hasSubmenu: false },
+  //   {
+  //     id: 2,
+  //     to: "",
+  //     label: "About",
+  //     hasSubmenu: true,
+  //     openKey: "about",
+  //     submenu: [
+  //       { id: 21, to: "autobiography", label: "Auto Biography" },
+  //       { id: 22, to: "yendhrakovil", label: "Yendhra Kovil" },
+  //     ],
+  //   },
+  //   {
+  //     id: 3,
+  //     to: "",
+  //     label: "Initiatives",
+  //     hasSubmenu: true,
+  //     openKey: "initiatives",
+  //     submenu: [
+  //       { id: 31, to: "activities", label: "Activities" },
+  //       { id: 32, to: "events", label: "Events" },
+  //       { id: 33, to: "workshops", label: "Workshops" },
+  //     ],
+  //   },
+  //   {
+  //     id: 4,
+  //     to: "book-my-dharshan",
+  //     label: "Book My Dharshan",
+  //     hasSubmenu: false,
+  //   },
+  //   { id: 5, to: "contact", label: "Contact", hasSubmenu: false },
+  //   { id: 6, to: "donate", label: "Donate", hasSubmenu: false },
+  // ];
+
+  return (
+    <header
+      className={`fixed top-0 left-0 w-full z-40 px-2 py-2 backdrop-blur-sm border-b border-zinc-200 ${
+        isMenuOpen ? "bg-yellow-50" : "bg-yellow-50 backdrop-blur-sm"
+      }`}
+    >
+      <div className="container">
+        <div className="flex items-center justify-between">
+          <div className="flex items-center justify-between flex-grow">
+            <div className="flex-shrink-0">
+              <NavLink to={"/"}>
+                <div className="flex items-center space-x-2 text-2xl font-medium">
+                  <img
+                    className="block w-20 h-20"
+                    src={jaikosha}
+                    alt="Jai Kosha"
+                  />
+                  <span className="text-red-500 font-bold">
+                    Jai Kosha <br />
+                    Foundation
+                  </span>
+                </div>
+              </NavLink>
+            </div>
+            <nav className="hidden lg:block">
+              <ul className="flex items-center gap-2">
+                {navLinks.map((link) => {
+                  const isSubmenuActive =
+                    link.hasSubmenu &&
+                    link.submenu.some(
+                      (sublink) => location.pathname === `/${sublink.to}`
+                    );
+                  return link.hasSubmenu ? (
+                    <li key={link.id} className="relative">
+                      <span
+                        className="relative"
+                        onMouseEnter={() => handleDropdown(link.openKey)}
+                        onMouseLeave={() => handleDropdown(link.openKey)}
+                      >
+                        <button
+                          className={`flex flex-row items-center px-3 py-2 ml-4 lg:ml-0 text-sm font-medium rounded-md focus:outline-none 
+                          ${
+                            isSubmenuActive
+                              ? "text-emerald-600 bg-yellow-100/50"
+                              : "text-zinc-600 hover:text-emerald-600 hover:bg-yellow-100/50"
+                          }`}
+                        >
+                          <span className="mx-2">{link.label}</span>
+                          <LuChevronDown />
+                        </button>
+                        <span
+                          className={`absolute right-0 w-32 mt-0 origin-top-right rounded-md shadow-lg bg-white
+                          transition-transform transform ease-out duration-100 
+                          ${
+                            openDropdown[link.openKey]
+                              ? "opacity-100 scale-100"
+                              : "opacity-0 scale-95 pointer-events-none"
+                          }`}
+                        >
+                          {/* Submenu */}
+                          <ul className="py-1 bg-yellow-100/30 rounded-md shadow-xs">
+                            {link.submenu.map((sublink) => (
+                              <li key={sublink.id}>
+                                <NavLink
+                                  to={sublink.to}
+                                  className="flex flex-row items-center px-4 py-2 text-sm font-medium text-gray-700 focus:text-gray-900 hover:text-gray-900 focus:outline-none hover:bg-yellow-100/50 focus:bg-yellow-100 whitespace-nowrap"
+                                >
+                                  {sublink.label}
+                                </NavLink>
+                              </li>
+                            ))}
+                          </ul>
+                        </span>
+                      </span>
+                    </li>
+                  ) : (
+                    <li key={link.id}>
+                      <NavLink
+                        to={link.to}
+                        className={({ isActive }) =>
+                          `${
+                            isActive
+                              ? "text-emerald-600 bg-yellow-100/70 shadow-sm"
+                              : "text-zinc-600 bg-transparent"
+                          } flex flex-row items-center p-2 ml-4 lg:ml-0 text-sm font-medium text-zinc-600 rounded-md hover:text-emerald-600 hover:bg-yellow-100/50 focus:outline-none focus:text-emerald-600`
+                        }
+                      >
+                        <span className="mx-2">{link.label}</span>
+                      </NavLink>
+                    </li>
+                  );
+                })}
+              </ul>
+            </nav>
+          </div>
+
+          <div className="flex lg:hidden">
+            <button
+              className="inline-flex items-center justify-center p-2 text-zinc-600 rounded-md hover:text-emerald-500 hover:bg-yellow-100/50 focus:outline-none focus:bg-emerald-100 focus:text-emerald-600"
+              onClick={toggleMenu}
+            >
+              <LuMenu className="text-3xl" />
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div className={`${isMenuOpen ? "block" : "hidden"} lg:hidden`}>
+        <div className="container">
+          <ul className="pt-2 pb-3">
+            {navLinks.map((link) =>
+              link.hasSubmenu ? (
+                <li key={link.id}>
+                  <span className="relative">
+                    <button
+                      className="w-full flex flex-row items-center p-2 text-base font-medium text-zinc-600 bg-transparent rounded-md focus:outline-none focus:text-emerald-600 focus:bg-yellow-100/50"
+                      onClick={() => handleDropdown(link.openKey)}
+                    >
+                      <span className="mx-2">{link.label}</span>
+                      <LuChevronDown />
+                    </button>
+                    <ul
+                      className={`${
+                        openDropdown[link.openKey] ? "block" : "hidden"
+                      } p-2 mb-1 bg-yellow-50 rounded-md shadow-xs`}
+                    >
+                      {link.submenu.map((sublink) => (
+                        <li
+                          key={sublink.id}
+                          onClick={() => setIsMenuOpen(false)}
+                        >
+                          <NavLink
+                            to={sublink.to}
+                            className={({ isActive }) =>
+                              `${
+                                isActive
+                                  ? "text-emerald-600 bg-yellow-100/70 shadow-sm"
+                                  : "text-zinc-600 bg-transparent"
+                              } flex flex-row items-center px-4 py-2 text-base font-medium rounded-md hover:text-emerald-600 hover:bg-yellow-100/50 focus:outline-none focus:text-emerald-600`
+                            }
+                          >
+                            {sublink.label}
+                          </NavLink>
+                        </li>
+                      ))}
+                    </ul>
+                  </span>
+                </li>
+              ) : (
+                <li key={link.id} onClick={() => setIsMenuOpen(false)}>
+                  <NavLink
+                    to={link.to}
+                    className={({ isActive }) =>
+                      `${
+                        isActive
+                          ? "text-emerald-600 bg-yellow-100/70 shadow-sm"
+                          : "text-zinc-600 bg-transparent"
+                      } flex flex-row items-center p-2 text-base font-medium rounded-md hover:text-emerald-600 hover:bg-yellow-100/50 focus:outline-none focus:text-emerald-600`
+                    }
+                  >
+                    <span className="ml-2">{link.label}</span>
+                  </NavLink>
+                </li>
+              )
+            )}
+          </ul>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
