730eb4d9dcee84fe927b00098ab85e9902610fdf
Mad-adz <madadzindia@gmail.com>
2025-03-22 12:07:45 +0530

    updated layout

--- client/src/layouts/AdminLayout.jsx
+++ client/src/layouts/AdminLayout.jsx
@@ -2,9 +2,11 @@ import { useSelector } from "react-redux";
 import { Navigate, Outlet } from "react-router-dom";
 
 const AdminLayout = () => {
-  const { user, error, status } = useSelector((state) => state.auth);
-  console.log({ user, error, status });
-  return user ? <Outlet /> : <Navigate to="/login" replace />;
+  // const { user, error, status } = useSelector((state) => state.auth);
+  const { user, token, isAuthenticated } = useSelector((state) => state.auth);
+
+  console.log({ user, token, isAuthenticated });
+  return isAuthenticated ? <Outlet /> : <Navigate to="/login" replace />;
 };
 
 export default AdminLayout;
--- client/src/layouts/AuthLayout.jsx
+++ client/src/layouts/AuthLayout.jsx
@@ -2,9 +2,10 @@ import { useSelector } from "react-redux";
 import { Navigate, Outlet } from "react-router-dom";
 
 const AuthLayout = () => {
-  const { user, error, status } = useSelector((state) => state.auth);
-  console.log({ user, error, status });
-  return !user ? <Outlet /> : <Navigate to="/" replace />;
+  const { isAuthenticated } = useSelector((state) => state.auth);
+  console.log({ isAuthenticated });
+
+  return !isAuthenticated ? <Outlet /> : <Navigate to="/" replace />;
 };
 
 export default AuthLayout;
