d5c3fc441a79febba9f1e82969cf72cb256d8735
Mad-adz <madadzindia@gmail.com>
2025-03-23 00:01:46 +0530

    updated dbconfig

--- server/src/config/mongo/dbConfig.js
+++ server/src/config/mongo/dbConfig.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectMongoDB = async () => {
   try {
     const db = await mongoose.connect(process.env.MONGODB_URI);
-    console.log(`connected to ${db.connection.host}`);
+    console.log(`connected to ${db.connection.host}:${db.connection.port}/${db.connection.name}`);
   } catch (error) {
     process.exit(1);
   }
