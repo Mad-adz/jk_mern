d6448d3873bb962cf17bccaa5aa4a94134b374e1
Mad-adz <madadzindia@gmail.com>
2025-03-22 15:36:30 +0530

    updated testing 6

--- server/src/app.js
+++ server/src/app.js
@@ -159,13 +159,13 @@ app.use(
 
 app.options("*", cors()); // Allow preflight requests for all routes
 
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "https://jaikosha-client-demo.vercel.app");
-//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-//   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-//   res.header("Access-Control-Allow-Credentials", "true");
-//   next();
-// });
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://jaikosha-client-demo.vercel.app");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
